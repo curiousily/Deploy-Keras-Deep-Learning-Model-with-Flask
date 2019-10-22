35f5e1cd91148e5cfc6c68b5c0a6e6aa44beeb85
Venelin Valkov <venelin@curiousily.com>
2019-10-22 13:34:33 +0300

    Add model & data preprocessor

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import joblib
+import pandas as pd
 from flask import Flask, jsonify, request
+from tensorflow import keras
 
 app = Flask(__name__)
+model = keras.models.load_model("assets/price_prediction_model.h5")
+transformer = joblib.load("assets/data_transformer.joblib")
 
 
 @app.route("/", methods=["POST"])
 def index():
-    content = request.json
-    return jsonify(content)
+    data = request.json
+    df = pd.DataFrame(data, index=[0])
+    prediction = model.predict(transformer.transform(df))
+    predicted_price = prediction.flatten()[0]
+    return jsonify({"price": str(predicted_price)})
--- test_api.py
+++ test_api.py
@@ -1,4 +1,15 @@
 import requests
 
-r = requests.post("http://localhost:5000", json={"key": "value"})
+data = {
+    "neighbourhood_group": "Brooklyn",
+    "latitude": 40.64749,
+    "longitude": -73.97237,
+    "room_type": "Private room",
+    "minimum_nights": 1,
+    "number_of_reviews": 9,
+    "calculated_host_listings_count": 6,
+    "availability_365": 365
+}
+
+r = requests.post("http://localhost:5000", json=data)
 print(r.json())
